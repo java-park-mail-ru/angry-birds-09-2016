609707f3b994c9eaf83bdcc233cccca6be4ddb3f
farid <arid1995@mail.ru>
2016-11-17 16:55:35 +0300

    package naming is fixed

---
+++ src/test/java/ru/mail/park/java/database/DatabaseTest.java
@@ -0,0 +1,11 @@
+package ru.mail.park.java.database;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by farid on 17.11.16.
+ */
+@RunWith(SpringRunner.class)
+public class DatabaseTest {
+}
